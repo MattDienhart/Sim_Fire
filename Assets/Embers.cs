b639d4094879ed33eac64b9921917815d2c21125
Ernest Kim <52210251+ernestk86@users.noreply.github.com>
2020-10-16 11:29:51 -0400

    Initial push wildfires

---
+++ Assets/Embers.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Embers : MonoBehaviour
+{
+    public int hitPoints;
+    public Tile * tile;
+    public string windDirection;
+    public int windSpeed;
+
+
+    //Constructor
+    public Embers(Tile * tileParameter, string direction, int speed)
+    {
+        hitPoints = 50;
+        tile = tileParameter;
+        windDirection = direction;
+        windSpeed = speed;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Wait 5 seconds
+        StartCoroutine(waiter(5));
+        //Check if adjacent tile can be lit aflame
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If fire dies
+        if (hitPoints <= 0) 
+        {
+            Destroy(this.Embers);
+        }
+    }
+
+    //Coroutine to make fire wait
+    IEnumerator waiter(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+
+    
+}
+
