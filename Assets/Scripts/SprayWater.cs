dc5e107406dbc65c965ee545d90e7072aa95866a
Matt Dienhart <matt.dienhart@gmail.com>
2020-10-24 15:37:30 -0400

    First commit for Week 4.

---
+++ Assets/Scripts/SprayWater.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SprayWater : MonoBehaviour
+{
+    private FireCrewData fireCrewData;
+    public List<GameObject> firesInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firesInRange = new List<GameObject>();
+        fireCrewData = gameObject.GetComponentInChildren<FireCrewData>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
