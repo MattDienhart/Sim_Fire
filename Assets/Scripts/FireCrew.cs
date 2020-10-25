4f5de0eba73fcc2ad0698995684c34f6e6e40003
Matt Dienhart <matt.dienhart@gmail.com>
2020-10-25 14:29:58 -0400

    Game Manager Added
    Created the Game Manger object and associated script. The Game Manager tracks the currently selected fire crew.

--- Assets/Scripts/FireCrew.cs
+++ Assets/Scripts/FireCrew.cs
@@ -6,16 +6,34 @@ public class FireCrew : MonoBehaviour
 {
     public int waterLevel;
     public int energyLevel;
+    private GameManagerBehavior gameManager;
+
+    public Sprite unselected;
+    public Sprite selected;
+    private SpriteRenderer crewSpriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        crewSpriteRenderer = GetComponent<SpriteRenderer>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // If this is not the currently selected object, update the sprite
+        if (gameManager.SelectedFireCrew != gameObject)
+        {
+            crewSpriteRenderer.sprite = unselected;
+        }
+    }
+
+    // Handle selection of this object
+    void OnMouseUp()
+    {
+        // If this is the currently selected game object, update the sprite
+        crewSpriteRenderer.sprite = selected;
+        gameManager.SelectedFireCrew = gameObject;
     }
 }
---
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerBehavior : MonoBehaviour
+{
+    private GameObject selectedFireCrew;
+    public GameObject SelectedFireCrew
+    {
+        get 
+        {
+            return selectedFireCrew;
+        }
+        set 
+        {
+            selectedFireCrew = value;
+            print("This object was selected: Fire Crew " + selectedFireCrew.GetInstanceID());
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
