79cc970b0f5a8eaae9681157f349a6e2c5e987f0
Kurt Kaiser <kurtbkaiser@gmail.com>
2020-10-25 17:39:55 -0400

    Created tilemap
    Used several different layers to create a grid and tilemaps, added get functions for tile neighbors

--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -10,6 +10,11 @@ public class TileScript : MonoBehaviour
     private bool burning = false;
     public Sprite[] sprites;
     List<GameObject> neighborTiles = new List<GameObject>();
+    private GameObject northTile;
+    private GameObject southTile;
+    private GameObject eastTile;
+    private GameObject westTile;
+
 
     private string terrian; // tile dependant? forest, hill, water, grass, road
     // private int elevation = 0; Maybe a feature for later
@@ -40,10 +45,26 @@ public class TileScript : MonoBehaviour
     private void getNeighbors()
     {
         int tileNum = System.Int32.Parse(Regex.Match(this.name, @"\d+").Value);
-        if(tileNum - 1 > 0) neighborTiles.Add(GameObject.Find("Tile (" + (tileNum - 1) + ")"));
-        if (tileNum + 1 < 180) neighborTiles.Add(GameObject.Find("Tile (" + (tileNum - 1) + ")"));
-        if (tileNum - 10 > 0) neighborTiles.Add(GameObject.Find("Tile (" + (tileNum - 1) + ")"));
-        if (tileNum + 10 < 180) neighborTiles.Add(GameObject.Find("Tile (" + (tileNum - 1) + ")"));
+        if (tileNum - 1 > 0)
+        {
+            westTile = GameObject.Find("Tile (" + (tileNum - 1) + ")");
+            neighborTiles.Add(westTile);
+        }
+        if (tileNum + 1 < 180)
+        {
+            eastTile = GameObject.Find("Tile (" + (tileNum - 1) + ")");
+            neighborTiles.Add(eastTile);
+        }
+        if (tileNum - 10 > 0)
+        {
+            southTile = GameObject.Find("Tile (" + (tileNum - 1) + ")");
+            neighborTiles.Add(southTile);
+        }
+        if (tileNum + 10 < 180)
+        {
+            northTile = GameObject.Find("Tile (" + (tileNum - 1) + ")");
+            neighborTiles.Add(northTile);
+        }
     }
 
     private void checkNeighbors()
@@ -59,4 +80,29 @@ public class TileScript : MonoBehaviour
             burning = true;
         }
     }
+
+    public GameObject GetNorth()
+    {
+        if(northTile) return northTile;
+        return this.gameObject;
+    }
+
+    public GameObject GetSouth()
+    {
+        if (northTile) return southTile;
+        return this.gameObject;
+    }
+
+    public GameObject GetEast()
+    {
+        if (northTile) return eastTile;
+        return this.gameObject;
+    }
+
+    public GameObject GetWest()
+    {
+        if (northTile) return westTile;
+        return this.gameObject;
+    }
+
 }
