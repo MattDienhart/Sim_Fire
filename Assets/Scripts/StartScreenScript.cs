25f0dea85627e0c30e94240172d87babbaa2e36f
Kurt Kaiser <kurtbkaiser@gmail.com>
2020-11-02 22:56:23 -0500

    Build v1 of game added

---
+++ Assets/Scripts/StartScreenScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartScreenScript : MonoBehaviour
+{
+    public Button startButton;
+    public GameObject[] fires;
+
+    void Start()
+    {
+        startButton.onClick.AddListener(() => StartClicked());
+    }
+
+    void StartClicked()
+    {
+        fires[0].GetComponent<SpriteRenderer>().enabled = true;
+        fires[1].GetComponent<SpriteRenderer>().enabled = true;
+        fires[2].GetComponent<SpriteRenderer>().enabled = true;
+        Invoke("NextScene", 1.0f);
+    }
+
+    void NextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
