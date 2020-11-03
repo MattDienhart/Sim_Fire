56f45d1a88e02b99aef4ec1cfc3a856d43a727dc
Kurt Kaiser <kurtbkaiser@gmail.com>
2020-11-03 11:06:19 -0500

    Police demo

---
+++ Assets/Scripts/PoliceScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceScript : MonoBehaviour
+{
+   
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = (pos);
+
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Debug.Log("Something was clicked:" + hit.collider.gameObject.name);
+            }
+        }
+    }
+}
